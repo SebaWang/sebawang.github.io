87feb50506a78cfc948e42e4741a5a1be87b0fcf
YenchiaChen <charlisyenchiachen@gmail.com>
2024-05-05 22:35:59 +0800

    fix: side nav error

--- src/Components/Component/SideNav.tsx
+++ src/Components/Component/SideNav.tsx
@@ -115,7 +115,7 @@ const SideNav: React.FC<Props> = ({ sections }) => {
       {isVisible && (
         <motion.div
           className={`fixed right-[1%] bottom-[30%] flex flex-col text-[#5A5A5A] font-light w-[250px] z-50 py-4 pl-10 bg-opacity-70 rounded-md transform-gpu transition-transform duration-1000 hidden ${
-            isVisible ? "md:block" : "hidden"
+            isVisible ? "md:flex" : "hidden"
           }`}
           style={{ fontSize: "90%"}}
           initial="hidden"
